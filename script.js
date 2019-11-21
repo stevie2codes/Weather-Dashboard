580425633ebdcfe0b50814b875a5bcf64222d1ab
stevie2codes <stevie2codes@gmail.com>
2019-11-21 15:41:20 -0600

    added search button and started js logic

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+let searchBox = $("#searchBox");
 
-$(document).ready(function(){
 
+$(document).ready(function () {
 
     let time = moment().format("LLLL");
-    console.log(time);
-
     $("#time").text(time);
 
+console.log("hi")
+    let myKey = "751635dd458149957afa00a64308bc08";
+
+    /*when user inputs a paramater a new li is added 
+    and the api is called then displayed in main card*/
+
+    let citySaver = text => {
+        let li = document.createElement('li');
+        li.textContent = text;
+        $("#cityList").append(li);
+    }
+
+    $("button").on("click",function(event){
+        event.preventDefault();
+        let userInput = $(this).val();
+        citySaver(userInput);
+        console.log("hello there");
+        
+    })
 
 
 
@@ -22,4 +40,4 @@ $(document).ready(function(){
 
 
 
-})
+});
