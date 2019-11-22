d3552ba7a409971a956f362334c9d2b1e056944c
stevie2codes <stevie2codes@gmail.com>
2019-11-22 12:05:59 -0600

    Started AJAX call to API and added Data to DOM. Set City, Date, img, and temp in kelvin so far

--- script.js
+++ script.js
@@ -3,29 +3,44 @@ let searchBox = $("#searchBox");
 
 $(document).ready(function () {
 
-    let time = moment().format("LLLL");
+    let time = moment().format("LL");
     $("#time").text(time);
 
-console.log("hi")
+
     let myKey = "751635dd458149957afa00a64308bc08";
 
     /*when user inputs a paramater a new li is added 
     and the api is called then displayed in main card*/
 
-    function citySaver(text){
-        let listEl = document.createElement("<li>");
-        listEl.text(searchBox).val();
-        $(listEl).append("#cityList");
-    }
+    
 
     
 
     $("button").on("click",function(event){
         event.preventDefault();
-        let userInput = $(this).val();
-        citySaver();
-        console.log("hello there");
-        
+        $(".figure").empty(); //empty search results upon each new search
+
+        let userInput = $(this).prev().val(); //getting value of user input
+        let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&APPID=" + myKey;
+        //calling the API
+       $.ajax({
+           url: queryURL,
+           method: "GET"
+       }).then(function(response){ 
+           console.log(response);
+           //setting the values to a new DOM element
+           let city = $("<h2>").text(`City: ${response.name}`);
+           let date = $("<h3>").text(`Date: ${time}`);
+           let iconImage = $("<img>").attr("src","http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+           let temp = $("<h4>").text(`Current temperature: ${response.main.temp}`) 
+           
+            //Appending the values to the figure box
+           $(".figure").append(city,date, iconImage,temp);
+
+
+       })
+
+       
     })
 
 
