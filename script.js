c942d01ae160a355bd5b22fcd342daee8b2d55c7
stevie2codes <stevie2codes@gmail.com>
2019-11-22 14:50:59 -0600

    added all parameters to display in main figure box

--- script.js
+++ script.js
@@ -6,15 +6,10 @@ $(document).ready(function () {
     let time = moment().format("LL");
     $("#time").text(time);
 
-
     let myKey = "751635dd458149957afa00a64308bc08";
 
-    /*when user inputs a paramater a new li is added 
-    and the api is called then displayed in main card*/
-
     
 
-    
 
     $("button").on("click",function(event){
         event.preventDefault();
@@ -28,14 +23,17 @@ $(document).ready(function () {
            method: "GET"
        }).then(function(response){ 
            console.log(response);
-           //setting the values to a new DOM element
-           let city = $("<h2>").text(`City: ${response.name}`);
-           let date = $("<h3>").text(`Date: ${time}`);
-           let iconImage = $("<img>").attr("src","http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-           let temp = $("<h4>").text(`Current temperature: ${response.main.temp}`) 
+           //setting the values to a new DOM element 
+           let city = $("<h1>").addClass("city-name").text(`City: ${response.name}`); 
+           let date = $("<h3>").addClass("date").text(`Date: ${time}`); 
+           let iconImage = $("<img>").addClass("icon-image").attr("src","http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+           let tempF = parseInt((response.main.temp - 273.15) * 1.8 + 32); //kelvin to farenheight Conversion
+           let temperature = $("<h4>").addClass("current-temp").text(`Current Temperature: ${tempF} F°`); 
+           let humidity = $("<h4>").addClass("humidity").text(`Humidity: ${response.main.humidity}%`);
+           let windSpeed = $("<h4>").addClass("wind-speed").text(`Wind Speed ${response.wind.speed}`)
            
             //Appending the values to the figure box
-           $(".figure").append(city,date, iconImage,temp);
+           $(".figure").append(city,iconImage, date, temperature, humidity,windSpeed);
 
 
        })
